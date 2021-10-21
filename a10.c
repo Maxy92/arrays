e3438b506122cb66d0b451cd80c4face8695a966
Maxy92 <83720281+Maxy92@users.noreply.github.com>
2021-10-21 09:47:23 -0300

    Add files via upload

---
+++ a10.c
@@ -0,0 +1,22 @@
+//Escribir un programa que imprima tres
+//strings representando una mano de
+//truco aleatoria, por ejemplo:
+//"Cuatro de copas", "Ancho de espada", "Tres de basto".
+
+#include "math.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "math.h"
+
+int main(void) {
+
+  char cartas[40];
+  char palo[4] = {"basto","espada","oro","copa"};
+
+
+
+
+
+
+  return 0;
+}
